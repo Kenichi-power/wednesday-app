b3f860d2fe0b9189f5dbff4def2db349d03512e9
Kenichi-power <kenes.baimukanov@gmail.com>
2022-09-15 13:27:06 +0500

    2

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -5,7 +5,7 @@ import MyButton from "../components/UI/button/MyButton";
 const PostForm = ({ create }) => {
   const [post, setPost] = useState({ title: "", body: "" });
 
-  const addNewPost = e => {
+  const addNewPost = (e) => {
     e.preventDefault();
     const newPost = {
       ...post,
@@ -19,17 +19,17 @@ const PostForm = ({ create }) => {
     <form>
       <MyInput
         value={post.title}
-        onChange={e => setPost({ ...post, title: e.target.value })}
+        onChange={(e) => setPost({ ...post, title: e.target.value })}
         type="text"
         placeholder="Post name"
       />
       <MyInput
         value={post.body}
-        onChange={e => setPost({ ...post, body: e.target.value })}
+        onChange={(e) => setPost({ ...post, body: e.target.value })}
         type="text"
-        placeholder="Post discription"
+        placeholder="Post description"
       />
-      {/* <MyInput ref={bodyInputRef} type="text" placeholder="Post discription" /> 
+      {/* <MyInput ref={bodyInputRef} type="text" placeholder="Post description" /> 
       неуправляемый компонент const bodyInputRef = useRef(); */}
       <MyButton onClick={addNewPost}>Create post</MyButton>
     </form>
